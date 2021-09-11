9fe7392014bc26d2a99735a12ffc1c7720047690
Chak Koppula <chak.koppula199@gmail.com>
2021-09-11 16:22:49 +0100

    updated HypixelStats class to include methods to obtain stats for all bedwars modes. also set up empty methods for other minigame stats

--- hypixel_stats.py
+++ hypixel_stats.py
@@ -44,8 +44,8 @@ class HypixelStats:
         bedwars_data = self.query_hypixel_api({'key': self.HYPIXEL_API_KEY, 'uuid':uuid})["player"]["stats"]["Bedwars"]
         game_data = {}
 
-        stats = ['kills', 'deaths', 'final_kills', 'final_deaths', 'beds_broken', 'beds_lost', 'wins', 'losses', 'iron_collected', 'gold_collected', 'diamonds_collected', 
-                'emeralds_collected', 'games_played']
+        stats = ['Kills', 'Deaths', 'Final Kills', 'Final Deaths', 'Beds Broken', 'Beds Lost', 'Wins', 'Losses', 'Iron Collected', 'Gold Collected', 'Diamonds Collected', 
+                'Emeralds Collected', 'Games Played']
 
         suffixes = ['_kills_bedwars', '_deaths_bedwars', '_final_kills_bedwars', '_final_deaths_bedwars', '_beds_broken_bedwars', '_beds_lost_bedwars', '_wins_bedwars',
                     '_losses_bedwars', '_iron_resources_collected_bedwars', '_gold_resources_collected_bedwars', '_diamond_resources_collected_bedwars',
@@ -59,28 +59,57 @@ class HypixelStats:
 
         return game_data
 
+    def bedwars_stats_text(self, game_data):
+        msg = ''
+        for key, value in game_data.items():
+            line = f'{key}: {value}\n'
+            msg += line
+
+        wins = game_data.get('Wins', None)
+        losses = game_data.get('Losses', None)
+        if wins is not None and losses is not None and losses != 0 and wins != 0:
+            win_percent = (wins / (wins + losses))* 100
+            msg += f'Win Percentage: {win_percent:.2f}%\n'
+
+        kills = game_data.get('Kills', None)
+        deaths = game_data.get('Deaths', None)
+        if kills is not None and deaths is not None and deaths != 0:
+            kd_ratio = kills / deaths
+            msg += f'K/D Ratio: {kd_ratio:.2f}\n'
+        return msg
+
 
     def bedwars_stats_solos(self, username):
         game_data = self.get_bedwars_data(username, 'eight_one')
-        return json.dumps(game_data, indent=4)
+        msg = self.bedwars_stats_text(game_data)
+        return msg
 
     def bedwars_stats_duos(self, username):
         game_data = self.get_bedwars_data(username, 'eight_two')
-        return json.dumps(game_data, indent=4)
+        msg = self.bedwars_stats_text(game_data)
+        return msg
 
     def bedwars_stats_trios(self, username):        
         game_data = self.get_bedwars_data(username, 'four_three')
-        return json.dumps(game_data, indent=4)
+        msg = self.bedwars_stats_text(game_data)
+        return msg
 
     def bedwars_stats_quads(self, username):
         game_data = self.get_bedwars_data(username, 'four_four')
-        return json.dumps(game_data, indent=4)
+        msg = self.bedwars_stats_text(game_data)
+        return msg
 
     def bedwars_stats_practice(self, username):
         uuid = self.get_uuid(username)
         bedwars_data = self.query_hypixel_api({'key': self.HYPIXEL_API_KEY, 'uuid':uuid})["player"]["stats"]["Bedwars"]["practice"]
         msg = json.dumps(bedwars_data, indent=4)
         return msg
+
+    def zombies_stats(self):
+        pass
+
+    def build_battle_stats(self):
+        pass
 
 # Used for debugging the above class
 if __name__ == "__main__":
